82fcb08a718ca789f355bbec0e9a2622e69800ac
Moe Nakamura <moe.aow124@gmail.com>
2024-04-03 20:02:38 +0900

    振替ファイル作成
    つかいわけ口座振替処理ファイル作成・口座、金額は固定値

--- back/app.js
+++ back/app.js
@@ -8,6 +8,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+
+const transferRouter = require('./routes/transfer'); //振替
+
 var app = express();
 
 // view engine setup
@@ -23,6 +27,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+app.use('/transfer', transferRouter);//振替
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
