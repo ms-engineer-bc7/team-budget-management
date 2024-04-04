4e96ff187954651f4b4da8ddae91c0671ffac3a1
tomoka <buru8buru8@gmail.com>
2024-04-05 00:08:39 +0900

    front修正

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,6 +1,8 @@
 "use client";
+
 import { useState, useEffect } from 'react';
-import { Heading, Flex, Box, Link, ChakraProvider } from '@chakra-ui/react';
+import { Image ,Heading, Flex, Box, Link, ChakraProvider ,useColorModeValue, Icon,Text} from '@chakra-ui/react';
+import { FaPiggyBank } from 'react-icons/fa';
 import Balance from '@/components/balance';
 import Transfer from '@/components/transfer';
 import AccountList from './accountList/page';
@@ -16,21 +18,38 @@ import axios from 'axios';
 // }
 
 export default function Home() {
-
+  const bgColor = useColorModeValue("pink.50", "purple.900");
+  const textColor = useColorModeValue("purple.700", "pink.200");
 
   return (
     <ChakraProvider>
-      <Flex direction="column" align="center" justify="center" margin="10">
-        <Box padding="5" width="70%" >
-          <Heading as="h2" size="xl" marginBottom="5" textAlign="center">
-            生活費予算管理
+          <Flex bgColor={bgColor} minHeight="100vh" direction="column" align="center" justify="center" padding="5">
+          <Flex direction="row" align="center" justify="center" mb="5">
+          <Image
+          src="/images/sunabar.png" // 画像のパスを指定
+          alt="subabarくま"
+          boxSize="200px" // 画像のサイズを設定
+          objectFit="cover" // 画像の比率を保ちながら、指定したボックスに合わせて表示
+          mr="4"
+        />
+         <Box bg="lightblue" p="4" borderRadius="lg" position="relative" >
+         <Text fontSize="lg">一週間ありがとうございました！</Text>
+         <Box position="absolute" left="-20px" top="50%" transform="translateY(-50%)" width="0" height="0" borderStyle="solid" borderColor="transparent lightblue transparent transparent" borderWidth="10px" />
+         </Box>
+        </Flex>
+        <Box bg="white" shadow="md" borderRadius="lg" padding="6" width={["95%", "85%", "70%"]}>
+          <Flex direction="column" align="center" justify="center">
+            <Icon as={FaPiggyBank} w={10} h={10} color="pink.400" />
+            <Heading as="h2" size="xl" color={textColor} marginBottom="4" textAlign="center">
+            生活費予算管理簿
           </Heading>
-          <Heading as="h3" fontSize="20" marginBottom="5" textAlign="center">
-          <Balance />
-          <Transfer />
-             つかいわけ口座一覧
-          </Heading> 
-          <AccountList />
+          <Heading as="h3" fontSize="lg" color="gray.600" marginBottom="5" textAlign="center">
+              <Balance /> 
+              <Transfer />
+             項目（つかいわけ口座CheckList）
+          </Heading>
+             <AccountList />
+          </Flex>
         </Box>
       </Flex>
     </ChakraProvider>
